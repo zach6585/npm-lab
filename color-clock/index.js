281cc11b020a7e5f14fd3fad75e299a8f9ab6e48
zach6585 <zmines@wesleyan.com>
2021-02-20 18:54:12 -0500

    Done.

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,6 +1,6 @@
 //No need to do any work here.  
 //If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
-//aaa
+//aaaa
 function displayTime() {
   let clock = document.querySelector('#clock')
   clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
